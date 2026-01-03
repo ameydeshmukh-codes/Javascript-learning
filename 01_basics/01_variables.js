c9750354b247fe8c16b63a5e0ca8cda2b544ffde
ameydeshmukh-codes <ameydeshmukh.work@gmail.com>
2026-01-03 16:51:11 +0000

    basics of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14553
+let accountEmail= "ameyswork@gmail.com"
+var accountPassword = "221144"
+accountCity = "Pune"
+
+// accountId = 2//not allowed
+/*
+
+prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+ accountEmail= "ameyswork11@gmail.com"
+ accountPassword = "123567"
+ accountCity = "mumbai"
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+
