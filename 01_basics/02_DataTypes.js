dc2baad31dcb64386494e1a6c8d5ca9818566124
Amey Deshmukh <ameydeshmukh.work@gmail.com>
2026-01-04 23:29:13 +0530

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert(5+5) // we are using nodejs ,not browser
+
+
+console.log(3 + 3)
+
+console.log("Amey")
+
+let name = "Amey"
+let age = 22
+
+let isLoggedIn = false
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined
+// symbol
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
+
+
+
+
+
+
+
